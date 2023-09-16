d29f4cfe5d28323e6566a91a909ea6761e8b4fea
Jacob Kolding <dacobi@gmail.com>
2023-09-16 12:18:03 +0200

    waypoint.cpp update, now takes text file input

--- tools/waypoint.cpp
+++ tools/waypoint.cpp
@@ -3,43 +3,92 @@
 #include <cmath>
 #include <iostream>
 #include <fstream>
+#include <filesystem>
 
-#include "waypoints.h"
+namespace fs = std::filesystem;
 
-int main(){
+struct mWaypoint{
+	unsigned char mBytes[3];
+};
 
-	std::ofstream outfile("waypoints1.bin", std::ios::binary );
+int main(int argc, char* argv[]){
+	
+	if((argc != 2) || !(fs::exists(fs::status(std::string(argv[1]))))){
+		std::cout << "Usage: " << std::string(argv[0]) << " <waypoint file>" << std::endl;
+		return 1;
+	}
+	
+	std::string mWayPath = std::string(argv[1]);
+	
+	std::ifstream input( mWayPath, std::ios::in );
+	
+	std::vector<mWaypoint> mWaypoints;
+	unsigned char mWayNum = 0;
+	
+
+
+	std::string nline;
+    	std::string ntmp1;    
+	std::string ntmp2;
+		    
+	int mx, my, mc;
+    
+    	while(!input.eof()){
+		std::getline(input, nline);
+
+		if(input.eof()){
+			break;
+		}
+		
+		mWayNum++;
+
+		ntmp2 = nline;
+
+		std::stringstream conwc(ntmp2); 
+
+		int wordc = 0;
+
+		while(conwc >> ntmp1){
+			wordc++;			
+		}       	
+    		
+		if((wordc < 3) || (wordc > 5)){
+			std::cout << "Error in file: \"" <<  mWayPath << "\"" << std::endl;		
+			return 1;
+		}
+
+		std::stringstream convert(nline); 
+
+		convert >> mx >> my >> mc;
+		
+		mWaypoint cWaypoint;
+		
+		mWaypoints.push_back(cWaypoint);
+
+		mWaypoints[mWaypoints.size()-1].mBytes[0] = (unsigned char)mx;
+		mWaypoints[mWaypoints.size()-1].mBytes[1] = (unsigned char)my;
+		mWaypoints[mWaypoints.size()-1].mBytes[2] = (unsigned char)mc;				
+	}    
+            	
+    	
+
+	std::ofstream outfile(std::string(mWayPath + ".bin"), std::ios::binary );
 
 	std::vector<unsigned char> obuffer;
 
-	obuffer.push_back(points_level1);
+	obuffer.push_back(mWayNum);
 	
-	for(int wi = 0; wi < points_level1; wi++){
-		obuffer.push_back(way_level1[wi][0]);			
-		obuffer.push_back(way_level1[wi][1]);	
-		obuffer.push_back(way_level1[wi][2]);			
+	for(int wi = 0; wi < mWayNum; wi++){
+		obuffer.push_back(mWaypoints[wi].mBytes[0]);			
+		obuffer.push_back(mWaypoints[wi].mBytes[1]);	
+		obuffer.push_back(mWaypoints[wi].mBytes[2]);			
 	}
 	
 	outfile.write((char*)obuffer.data(),obuffer.size());
 	outfile.close();
-
-	std::ofstream outfile2("waypoints2.bin", std::ios::binary );
-
-	std::vector<unsigned char> obuffer2;
-
-	obuffer2.push_back(points_level2);
-	
-	for(int wi = 0; wi < points_level2; wi++){
-		obuffer2.push_back(way_level2[wi][0]);			
-		obuffer2.push_back(way_level2[wi][1]);	
-		obuffer2.push_back(way_level2[wi][2]);			
-	}
-	
-	outfile2.write((char*)obuffer2.data(),obuffer2.size());
-	outfile2.close();
-
-	
-	
+		
+	std::cout << "\"" <<  std::string(mWayPath + ".bin") << "\" has been written" << std::endl;		
+		
 	return 0;
 }
 

f4d7ee54fa0b72a6ca027ca6f915e1b96d4fdf9a
Jacob Kolding <dacobi@gmail.com>
2023-09-17 09:44:51 +0200

    makegame.ps1 update

--- makegame.ps1
+++ makegame.ps1
@@ -7,8 +7,34 @@ param([String]$Track)
 $folder = $tracks + "\" + $track
 $tracknum = $track[-1]
 
-$curpath = "DAT\TLST" + $tracknum
 $curfile = $folder + "\tiles.bin"
+$curpath = "DAT\TLST" + $tracknum
+
+Copy-Item -Path $curfile -Destination $curpath
+
+$curfile = $folder + "\map0.bin"
+$curpath = "DAT\TRK" + $tracknum
+
+Copy-Item -Path $curfile -Destination $curpath
+
+$curfile = $folder + "\colmap0.bin"
+$curpath = "DAT\CLM" + $tracknum
+
+Copy-Item -Path $curfile -Destination $curpath
+
+$curfile = $folder + "\pal.bin"
+$curpath = "DAT\PAL" + $tracknum
+
+Copy-Item -Path $curfile -Destination $curpath
+
+$curpath = "tools\waypoint.t" + $tracknum
+$curfile = "tools\waypoints.exe"
+
+
+Invoke-Expression "$curfile $curpath"
+
+$curfile = "tools\waypoint.t" + $tracknum + ".bin"
+$curpath = "DAT\WAY" + $tracknum
 
 Copy-Item -Path $curfile -Destination $curpath
 
